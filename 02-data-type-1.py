002d94f33e9e9dd9b3c0cbfa1a26504649c372b5
JH.KIM <jh.kim@JH-MacBook-Pro.local>
2018-06-13 14:27:16 +0900

    정수, 소수, 문자열

---
+++ 02-data-type-1.py
@@ -0,0 +1,21 @@
+# 정수 int -- integer
+print(1+1)
+# 소수 float
+print(1.5+2.1)
+# 문자 str -- string
+print("hello")
+
+# 문자열 더하기
+print("hello" + "1")
+
+# 정수/소수간 변경
+print(4//2)
+print(int(4/2))
+print(float(2))
+
+# 문자열/숫자간 변경
+print(int("1"))
+print("1")
+# 위의 두개는 같은 값을 출력하지만 같지는 않다
+print(int("1.5"))
+# 1이 출력될줄 알았는데, 에러가 남
