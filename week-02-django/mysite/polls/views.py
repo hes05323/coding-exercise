a2c8df783d26a481ca7646566d3b08c75c74b274
JH.KIM <jh.kim@JH-MacBook-Pro.local>
2018-06-16 19:50:14 +0900

    html, css basic

--- week-02-django/mysite/polls/views.py
+++ week-02-django/mysite/polls/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+# from random import randint
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    # random_number = randint(1, 10)
+    # return HttpResponse("Hello, world. {}".format(random_number))
+    # return HttpResponse("Hello, world. You're at the index.")
+    name = "irene"
+    return render(request, "index.html", { "name" : name })
