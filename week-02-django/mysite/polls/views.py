a65a18e990366b0e61341a3794d9e2d9f8a984ca
JH.KIM <jh.kim@JH-MacBook-Pro.local>
2018-06-16 22:12:04 +0900

    learn bootstrap

--- week-02-django/mysite/polls/views.py
+++ week-02-django/mysite/polls/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 # from random import randint
+from .models import Article
 
 
 def index(request):
     # random_number = randint(1, 10)
     # return HttpResponse("Hello, world. {}".format(random_number))
     # return HttpResponse("Hello, world. You're at the index.")
-    name = "irene"
-    return render(request, "index.html", { "name" : name })
+    # name = "irene"
+    # return render(request, "index.html", { "name" : name })
+    article_list = Article.objects.all()
+    # Article.objects.create(
+    #     title="hello",
+    #     contents="this is test",
+    #     view_count=0
+    # )
+    ctx = {
+        "article_list" : article_list
+    }
+    return render(request, "index.html", ctx)
